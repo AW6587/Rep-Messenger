cebfade123ea7add14e9ef1c6086ca6ad5203f9d
Midori-W <36549707+Midori-W@users.noreply.github.com>
2021-03-04 16:24:50 -0700

    Update AboutActivity.java
    added dynamic version updating and set hyperlinks to clickable

--- app/src/main/java/com/example/repmessenger/AboutActivity.java
+++ app/src/main/java/com/example/repmessenger/AboutActivity.java
@@ -10,5 +10,18 @@ public class AboutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+        
+        TextView aboutTitle = findViewById(R.id.textView_about_title);
+        
+        try {
+            String versionNum = this.getPackageManager().getPackageInfo(this.getPackageName(), 0).versionName;
+            String formatted = getString(R.string.about_title, versionNum);
+            aboutTitle.setText(formatted);
+        } catch (NameNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        TextView aboutBody = findViewById(R.id.textView_about_body);
+        aboutBody.setMovementMethod(LinkMovementMethod.getInstance());
     }
 }
